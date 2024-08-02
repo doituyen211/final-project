30767144114532001ab8b43707f871a718865421
tungched <thuyluct4@gmail.com>
2024-08-02 16:12:32 +0700

    update

---
+++ src/components/FormComponent/index.jsx
@@ -0,0 +1,147 @@
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+import SelectDropdown from "../SelectDownButton";
+import axios from "axios";
+import {Button, Col, Form, Row} from "react-bootstrap";
+import Input from "../InputComponents";
+
+function FormComponent({fields, onSubmit, isEdit, idCurrent, onClose, isView}) {
+    const [formData, setFormData] = useState(
+        fields.reduce((acc, field) => ({...acc, [field.name]: ''}), {})
+    );
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData((prev) => ({...prev, [name]: value}));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(formData);
+    };
+
+    useEffect(() => {
+        console.log(isView)
+        if (isEdit || isView) {
+            axios.get(`https://66aa0b5b613eced4eba7559a.mockapi.io/subject/${idCurrent}`)
+                .then((res) => {
+                    setFormData(res.data);
+                })
+                .catch((err) => {
+                    console.error("Error fetching data:", err);
+                });
+        }
+    }, [isEdit, idCurrent, isView]);
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                {fields.map((field) => {
+                    switch (field.type) {
+                        case 'text':
+                            return (
+                                <Col key={field.name} md={6} className="mb-3">
+                                    <Form.Group controlId={field.name}>
+                                        <Form.Label>{field.label}</Form.Label>
+                                        <Input
+                                            type="text"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={formData[field.name] || ''}
+                                            onChange={handleChange}
+                                            placeholder={field.placeholder}
+                                            className="form-control"
+                                            disabled={isView}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            );
+                        case 'select':
+                            return (
+                                <Col key={field.name} md={6} className="mb-3">
+                                    <Form.Group controlId={field.name}>
+                                        {/*<Form.Label>{field.label}</Form.Label>*/}
+                                        <SelectDropdown
+                                            id={field.name}
+                                            apiUrl={field.apiUrl}
+                                            label={field.label}
+                                            defaultOption={isEdit || isView ? {
+                                                value: formData[field.name],
+                                                label: formData[field.name]
+                                            } : field.defaultOption}
+                                            onChange={(e) => handleChange(e)}
+                                            disabled={isView}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            );
+                        case 'date':
+                            return (
+                                <Col key={field.name} md={6} className="mb-3">
+                                    <Form.Group controlId={field.name}>
+                                        <Form.Label>{field.label}</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={formData[field.name]}
+                                            onChange={handleChange}
+                                            disabled={isView}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            );
+                        case 'number':
+                            return (
+                                <Col key={field.name} md={6} className="mb-3">
+                                    <Form.Group controlId={field.name}>
+                                        <Form.Label>{field.label}</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={formData[field.name]}
+                                            onChange={handleChange}
+                                            placeholder={field.placeholder}
+                                            disabled={isView}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            );
+                        default:
+                            return null;
+                    }
+                })}
+            </Row>
+            <div className="d-flex justify-content-center">
+                <Button variant="secondary" className="me-2" type="button" onClick={onClose}>Close</Button>
+                {isView ? <Button variant="primary" type="submit">Edit</Button> :
+                    <Button variant="primary" type="submit">Save Changes</Button>}
+            </div>
+        </Form>
+    );
+}
+
+FormComponent.propTypes = {
+    fields: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            type: PropTypes.oneOf(['text', 'select', 'date', 'number']).isRequired,
+            label: PropTypes.string.isRequired,
+            placeholder: PropTypes.string,
+            apiUrl: PropTypes.string,  // Add apiUrl for select fields
+            defaultOption: PropTypes.shape({
+                value: PropTypes.string.isRequired,
+                label: PropTypes.string,
+            }),  // Add defaultOption for select fields
+        })
+    ).isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    isEdit: PropTypes.bool.isRequired,
+    idCurrent: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+};
+
+export default FormComponent;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import UserComponent from "./UserComponent";
+import SubjectComponent from "./SubjectComponent";
+import ReservationComponent from "./ReservationComponent";
+import BookComponent from "./BookComponent";
+
+export {
+    UserComponent,
+    SubjectComponent,
+    ReservationComponent,
+    BookComponent
+}
