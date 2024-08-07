57cc47e0236149032704bff847a850ace6190bd7
Lê Hoài Nam <namlh.greenacademy@gmail.com>
2024-08-07 09:38:09 +0000

    Revert "Merge branch 'feature/phubv/quanlylichthi_2' into 'crm_education'"
    This reverts merge request !116

--- src/components/FormInputComponents/index.jsx
+++ src/components/FormInputComponents/index.jsx
@@ -1,49 +1,38 @@
-import React from "react";
-import PropTypes from "prop-types";
+// src/components/FormInputComponents.js
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const FormInput = ({
-  label,
-  type,
-  name,
-  value,
-  onChange,
-  placeholder,
-  error,
-  disabled,
-}) => {
-  return (
-    <div className="form-group">
-      <label>{label}</label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className={`form-control ${error ? "is-invalid" : ""}`}
-        disabled={disabled}
-      />
-      {error && <div className="invalid-feedback">{error}</div>}
-    </div>
-  );
+const FormInput = ({label, type, name, value, onChange, placeholder, error}) => {
+    return (
+        <div className="form-group">
+            <label>{label}</label>
+            <input
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className={`form-control ${error ? 'is-invalid' : ''}`}
+            />
+            {error && <div className="invalid-feedback">{error}</div>}
+        </div>
+    );
 };
 
 FormInput.propTypes = {
-  label: PropTypes.string.isRequired,
-  type: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  error: PropTypes.string,
-  disabled: PropTypes.bool,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    error: PropTypes.string
 };
 
 FormInput.defaultProps = {
-  type: "text",
-  placeholder: "",
-  error: "",
-  disabled: false,
+    type: 'text',
+    placeholder: '',
+    error: ''
 };
 
 export default FormInput;
--- src/components/TableComponent/index.jsx
+++ src/components/TableComponent/index.jsx
@@ -1,111 +1,93 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { BsEye, BsPencil, BsTrash } from "react-icons/bs"; // Import Bootstrap icons
+import { BsEye, BsPencil, BsTrash } from 'react-icons/bs'; // Import Bootstrap icons
 
 function TableComponents(props) {
-  const {
-    cols,
-    titleTable,
-    dataTable,
-    classTable,
-    formFieldsProp,
-    actionView,
-    actionEdit,
-    useModal,
-    actionDelete,
-    openModal,
-    currentPage,
-  } = props;
+    const {
+        cols,
+        titleTable,
+        dataTable,
+        classTable,
+        formFieldsProp,
+        actionView,
+        actionEdit,
+        useModal,
+        actionDelete,
+        openModal,
+        currentPage,
+    } = props;
 
-  return (
-    <>
-      <h2>{titleTable}</h2>
-      <table className={classTable}>
-        <thead>
-          <tr>
-            {Array.isArray(cols) &&
-              cols.map((col, index) => (
-                <th key={index} style={{ textAlign: "center" }}>
-                  {col}
-                </th>
-              ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dataTable.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              <td>{rowIndex + 10 * (currentPage - 1) + 1}</td>
-              {/*<td>{row.id}</td>*/}
-              {formFieldsProp.map((field, cellIndex) => (
-                <td key={cellIndex}>{row[field.name]}</td>
-              ))}
-              <td className="text-center">
-                <Button
-                  variant="light"
-                  className="me-2"
-                  onClick={() => {
-                    if (!useModal) {
-                      console.log("Using actionView");
-                      actionView(row);
-                    } else {
-                      console.log("Using default view action");
-                      openModal("VIEW", true, row);
-                    }
-                  }}
-                >
-                  <BsEye />
-                </Button>
-                <Button
-                  variant="primary"
-                  className="me-2"
-                  onClick={() => {
-                    if (!useModal) {
-                      console.log("Using actionEdit");
-                      actionEdit(row);
-                    } else {
-                      console.log("Using default edit action");
-                      openModal("EDIT", true, row);
-                    }
-                  }}
-                >
-                  <BsPencil />
-                </Button>
+    return (
+        <>
+            <h2>{titleTable}</h2>
+            <table className={classTable}>
+                <thead>
+                    <tr>
+                        {Array.isArray(cols) &&
+                            cols.map((col, index) => (
+                                <th key={index} style={{ textAlign: "center" }}>
+                                    {col}
+                                </th>
+                            ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataTable.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            <td>{rowIndex + 10 * (currentPage - 1) + 1}</td>
+                            {/*<td>{row.id}</td>*/}
+                            {formFieldsProp.map((field, cellIndex) => (
+                                <td key={cellIndex}>{row[field.name]}</td>
+                            ))}
+                            <td className="text-center">
+                                <Button
+                                    variant="light"
+                                    className="me-2"
+                                    onClick={() => {
+                                        if (!useModal) {
+                                            console.log("Using actionView");
+                                            actionView(row);
+                                        } else {
+                                            console.log(
+                                                "Using default view action"
+                                            );
+                                            openModal("VIEW", true, row);
+                                        }
+                                    }}
+                                >
+                                    <BsEye/>
+                                </Button>
+                                <Button
+                                    variant="primary"
+                                    className="me-2"
+                                    onClick={() => {
+                                        if (!useModal) {
+                                            console.log("Using actionEdit");
+                                            actionEdit(row);
+                                        } else {
+                                            console.log(
+                                                "Using default edit action"
+                                            );
+                                            openModal("EDIT", true, row);
+                                        }
+                                    }}
+                                >
+                                    <BsPencil/>
+                                </Button>
 
-                <Button variant="danger" onClick={() => actionDelete(row)}>
-                  <BsTrash />
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+                                <Button
+                                    variant="danger"
+                                    onClick={() => actionDelete(row)}
+                                >
+                                    <BsTrash/>
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
-
-TableComponents.propTypes = {
-  cols: PropTypes.arrayOf(PropTypes.string).isRequired,
-  titleTable: PropTypes.string.isRequired,
-  dataTable: PropTypes.arrayOf(PropTypes.object).isRequired,
-  classTable: PropTypes.string,
-  formFieldsProp: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  actionView: PropTypes.func.isRequired,
-  actionEdit: PropTypes.func.isRequired,
-  useModal: PropTypes.bool,
-  actionDelete: PropTypes.func.isRequired,
-  openModal: PropTypes.func.isRequired,
-  currentPage: PropTypes.number.isRequired,
-  isLoading: PropTypes.bool, // Add propTypes for isLoading
-};
-
-TableComponents.defaultProps = {
-  classTable: "table table-bordered table-hover",
-  useModal: true,
-  isLoading: false, // Default value for isLoading
-};
 
 export default TableComponents;
