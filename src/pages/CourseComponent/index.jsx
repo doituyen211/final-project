f3ae9f992569bf8decaea760ef1ca593e6e561fe
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-08-09 01:11:24 +0700

    Fix not find import FormComponent from "./FormComponent"

--- src/pages/CourseComponent/index.jsx
+++ src/pages/CourseComponent/index.jsx
@@ -5,7 +5,7 @@ import { Button, Form } from "react-bootstrap";
 import TableComponents from '../../components/TableComponent';
 // import SelectDropdown from '../../components/SelectDownButton';
 import DeleteComponent from "../../components/DeleteItemComponent";
-import FormComponent from "../../pages/CourseComponent/FormComponent";
+import FormComponent from "./FormComponent"
 import PagingComponent from "../../components/PagingComponent";
 import API from "../../store/Api";
 
