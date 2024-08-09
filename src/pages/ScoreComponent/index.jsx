3178ef56511e93e7ba083d284445c5e073917694
tuyendt <tuyendt@gmail.com>
2024-08-10 00:23:32 +0700

    adding update

--- src/pages/ScoreComponent/index.jsx
+++ src/pages/ScoreComponent/index.jsx
@@ -1,39 +1,20 @@
 import axios from "axios";
-import { Button, Table, Modal, Form, FormGroup } from "react-bootstrap";
+import { Table, Modal, Form, ModalHeader, ModalBody } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function ScoreComponent() {
   const [data, setData] = useState([]);
-
   const [dataRow, setDataRow] = useState({});
-
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   const [selectedId, setSelectedId] = useState(null);
   const [updateDataId, setUpdateDataId] = useState(null);
-
-  const [newData, setNewData] = useState({
-    name: "",
-    grade: [],
-    status: "",
-    examDate: null,
-    subject: [],
-  });
-
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-
-  // const handleNewData = (e) => {
-  //   setNewData({ ...newData});
-  // };
+  const [scoreNew, setNewScore] = useState([]);
 
   const Url = "https://66b2e33c7fba54a5b7eab653.mockapi.io/grades/grade";
-  /* param: id, name , grade, examDate, subject*/
-
-  const url = "https://freetestapi.com/api/v1/students ";
-  // param: id, name, age, gender, address, email, phone, courses, gpa, image
 
   useEffect(() => {
     fetchData();
@@ -50,58 +31,74 @@ export default function ScoreComponent() {
     }
   };
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
   if (error) return <p>Error occurred: {error.message}</p>;
 
   const handleViewData = (id) => {
     setSelectedId(id);
   };
+
   const handleShowUpdateForm = (data) => {
     setShow(true);
     setUpdateDataId(data.id);
     setDataRow(data);
+    setNewScore([...data.grade]);
   };
 
-  function handleAdd() {}
-
-  function handleEdit(id) {
-    try {
-      axios.put(`${URL}/${id}`, newData);
-      setData(
-        data.map((data) => (data.id === id ? { ...data, ...newData } : data))
-      );
-      setShow(false);
-      setUpdateDataId(null);
-      setNewData({
-        name: "",
-        grade: [],
-        status: "",
-        examDate: null,
-        subject: [],
-      });
-    } catch (error) {
-      console.error("Error updating data:", error);
-    }
-  }
+  const handleSaveNewScore = (e, index) => {
+    const value = e.target.value;
+    setNewScore((prevData) => {
+      const updatedScores = [...prevData];
+      updatedScores[index] = value;
+      return updatedScores;
+    });
+  };
 
   function handleDelete(id) {
     try {
       axios.delete(`${Url}/${id}`);
       setData(data.filter((data) => data.id !== id));
       setSelectedId(null);
+      toast.success("Record deleted successfully!");
     } catch (error) {
       console.error("Error deleting data:", error);
+      toast.error("Failed to delete the record. Please try again.");
+    }
+  }
+
+  function handleUpdateScore(e) {
+    e.preventDefault();
+    try {
+      axios.put(`${Url}/${updateDataId}`, { ...dataRow, grade: scoreNew });
+      setData((prevData) =>
+        prevData.map((item) =>
+          item.id === updateDataId ? { ...item, grade: scoreNew } : item
+        )
+      );
+      setUpdateDataId(null);
+      setShow(false);
+      toast.success("Record update successfully!");
+    } catch (error) {
+      console.error("Error updating data:", error);
+      toast.error("Failed to update the record. Please try again.");
     }
   }
 
   return (
     <div>
-      <div className="mr-2 ">
-        <div style={{ fontWeight: 600, fontSize: 24 }}>Quản Lý Điểm</div>
-        <Button variant="success">
-          <img src="" alt="Add" onClick={handleAdd} />
-        </Button>
-      </div>
       <div className="container mt-4">
         <div className="row row-cols-2">
           <div className="col-sm-6">
@@ -113,103 +110,93 @@ export default function ScoreComponent() {
               handleShowUpdateForm={handleShowUpdateForm}
             />
           </div>
-          {selectedId && (
-            <div className="col">
-              <Infor data={data} selectedId={selectedId} />
-            </div>
-          )}
-          {selectedId && (
-            <div className="col">
-              <Detail data={data} selectedId={selectedId} />
-            </div>
-          )}
+          <div className="col-sm-5">
+            {selectedId && (
+              <>
+                <Infor data={data} selectedId={selectedId} />
+                <Detail data={data} selectedId={selectedId} />
+              </>
+            )}
+          </div>
           {show && updateDataId && (
             <UpdateForm
               show={show}
-              handleClose={handleClose}
-              handleSave={handleEdit}
+              handleClose={() => setShow(false)}
               dataRow={dataRow}
-              updateDataId={updateDataId}
-              setNewData={setNewData}
-              newData={newData}
-              // handleNewData={handleNewData}
+              handleSaveNewScore={handleSaveNewScore}
+              handleUpdateScore={handleUpdateScore}
+              scoreNew={scoreNew}
             />
           )}
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 }
 
 function Infor({ data, selectedId }) {
-  const reData = data.filter((data, i) => data.id === selectedId);
-  const { name, classes, image } = reData[0];
+  const reData = data.find((item) => item.id === selectedId);
+  const { name, classes, image } = reData;
 
   return (
     <div>
       <span style={{ fontWeight: 450, fontSize: 20 }}>Infor</span>
-      <div>
-        <Table bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Class</th>
-              <th>Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{selectedId}</td>
-              <td>{name}</td>
-              <td>{classes}</td>
-              <td>{image}</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th style={{ width: "4%", border: "1px solid black" }}>Id</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Name</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Class</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid black" }}>{selectedId}</td>
+            <td style={{ border: "1px solid black" }}>{name}</td>
+            <td style={{ border: "1px solid black" }}>{classes}</td>
+            <td style={{ border: "1px solid black" }}>{image}</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
 
 function Detail({ data, selectedId }) {
-  const reData = data.filter((data, i) => data.id === selectedId);
-  const { name, grade, status, examDate, subject } = reData[0];
+  const reData = data.find((item) => item.id === selectedId);
+  const { grade, status, examDate, subject } = reData;
+
   return (
     <div>
       <span style={{ fontWeight: 450, fontSize: 20 }}>Detail</span>
-      <div>
-        <Table bordered>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Subject</th>
-              <th>Status</th>
-              <th>Exam Date</th>
-              <th>Scores</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{selectedId}</td>
-              <td>{name}</td>
-              {subject.map((sub, i) => (
-                <tr key={i}>
-                  <td>{sub}</td>
-                </tr>
-              ))}
-              <td>{status}</td>
-              <td>{examDate}</td>
+      <Table bordered>
+        <thead>
+          <tr>
+            <th style={{ width: "4%", border: "1px solid black" }}>Id</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Subject</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Status</th>
+            <th style={{ width: "4%", border: "1px solid black" }}>
+              Exam Date
+            </th>
+            <th style={{ width: "4%", border: "1px solid black" }}>Scores</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid black" }}>{selectedId}</td>
+            <td style={{ border: "1px solid black" }}>{subject.join(", ")}</td>
+            <td style={{ border: "1px solid black" }}>{status}</td>
+            <td style={{ border: "1px solid black" }}>{examDate}</td>
+            <td style={{ border: "1px solid black" }}>
               {grade.map((score, i) => (
-                <tr key={i}>
-                  <td>{score}</td>
-                </tr>
+                <div key={i}>{score}</div>
               ))}
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
@@ -218,52 +205,54 @@ function General({ data, handleViewData, handleDelete, handleShowUpdateForm }) {
   return (
     <div>
       <div style={{ fontWeight: 450, fontSize: 20 }}>General</div>
-      <div>
-        <Table bordered hover scrolled>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Score</th>
-              <th>Status</th>
-              <th></th>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th style={{ height: "4%", border: "1px solid black" }}>Id</th>
+            <th style={{ height: "4%", border: "1px solid black" }}>Name</th>
+            <th style={{ height: "4%", border: "1px solid black" }}>Score</th>
+            <th style={{ height: "4%", border: "1px solid black" }}>Status</th>
+            <th
+              style={{ height: "4%", weight: "4%", border: "1px solid black" }}
+            >
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td style={{ border: "1px solid black" }}>{item.id}</td>
+              <td style={{ border: "1px solid black" }}>{item.name}</td>
+              <td style={{ border: "1px solid black" }}>
+                {item.grade.reduce((sum, num) => sum + Number(num), 0) /
+                  item.grade.length}
+              </td>
+              <td style={{ border: "1px solid black" }}>{item.status}</td>
+              <td style={{ border: "1px solid black" }}>
+                <button
+                  className="m-2 btn btn-secondary btn-sm"
+                  onClick={() => handleViewData(item.id)}
+                >
+                  <i className="bi bi-eye"></i>
+                </button>
+                <button
+                  className="m-2 btn btn-success btn-sm"
+                  onClick={() => handleShowUpdateForm(item)}
+                >
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button
+                  className="m-2 btn btn-danger btn-sm"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  <i className="bi bi-x-octagon"></i>
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {data.map((data, index) => (
-              <tr key={index}>
-                <td>{data.id}</td>
-                <td>{data.name}</td>
-                <td>{data.grade}</td>
-                <td>{data.status}</td>
-                <td>
-                  <Button className="m-2" variant="danger">
-                    <img
-                      src="delete.png"
-                      alt="delete"
-                      onClick={() => handleDelete(data.id)}
-                    />
-                  </Button>
-                  <Button className="m-2" variant="success">
-                    <img
-                      src="delete.png"
-                      alt="view"
-                      onClick={() => handleViewData(data.id)}
-                    />
-                  </Button>
-                  <Button variant="success">
-                    <img
-                      src="edit.png"
-                      alt="edit"
-                      onClick={() => handleShowUpdateForm(data)}
-                    />
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
@@ -272,107 +261,64 @@ function UpdateForm({
   show,
   handleClose,
   dataRow,
-  handleSave,
-  updateDataId,
-  handleNewData,
-  newData,
-  setNewData,
+  handleSaveNewScore,
+  handleUpdateScore,
+  scoreNew,
 }) {
-  // const reData = data.filter((data, i) => data.id === updateDataId);
-  // const { name, grade, status, examDate, subject } = reData[0];
-  // console.log(updateDataId);
-  // console.log(dataRow);
-
-  const updateField = (field, value) => {
-    setNewData((prevData) => ({
-      ...prevData,
-      [field]: value,
-    }));
-  };
-
   return (
     <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Update</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="container">
-        <Form className="row row-cols-2">
+      <ModalHeader closeButton>
+        <span>Update</span>
+      </ModalHeader>
+      <ModalBody className="container">
+        <Form className="row row-cols-2" onSubmit={handleUpdateScore}>
           <div className="col">
             <span>Infor</span>
-            <Form.Group className="mb-3">
-              <Form.Label>Id</Form.Label>
-              <Form.Control type="text" placeholder={updateDataId} disabled />
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                // placeholder={name}
-                value={newData.name || dataRow.name}
-                onChange={(e) => updateField("name", e.target.value)}
-              />
-              <Form.Label>Status</Form.Label>
-              <Form.Control
-                type="text"
-                // placeholder={status}
-                onChange={(e) =>
-                  setNewData({ ...newData, status: e.target.value })
-                }
-              />
-            </Form.Group>
+            <div className="mb-3">
+              <label className="m-2">Id</label>
+              <input type="text" value={dataRow.id} disabled />
+              <label className="m-2">Name</label>
+              <input type="text" value={dataRow.name} disabled />
+              <label className="m-2">Status</label>
+              <input type="text" value={dataRow.status} disabled />
+            </div>
           </div>
           <div className="col">
             <span>Detail</span>
-            <Form.Group className="mb-3">
-              <Form.Label>Id</Form.Label>
-              <Form.Control type="text" placeholder={updateDataId} disabled />
-              <Form.Label>Subject</Form.Label>
-              {/* {subject.map((sub, i) => (
-                <FormGroup>
-                  <Form.Control
-                    type="text"
-                    placeholder={sub}
-                    onChange={(e) =>
-                      setNewData({ ...newData, subject: e.target.value })
-                    }
-                  />
-                </FormGroup>
-              ))} */}
-              <Form.Label>Exam Date</Form.Label>
-              <Form.Control
-                type="text"
-                // placeholder={examDate}
-                value={dataRow.name}
-                onChange={(e) =>
-                  setNewData({ ...newData, examDate: e.target.value })
-                }
-              />
-              <Form.Label>Score</Form.Label>
-              {dataRow.grade.map((score, i) => (
-                <FormGroup>
-                  <Form.Control
-                    type="number"
-                    // placeholder={score}
-                    value={score}
-                    onChange={(e) =>
-                      setNewData({ ...newData, score: e.target.value })
-                    }
-                  />
-                </FormGroup>
+            <div className="mb-3">
+              <label className="m-2">Subject</label>
+              {dataRow.subject.map((sub, i) => (
+                <div className="m-2" key={i}>
+                  <input type="text" value={sub} disabled />
+                </div>
               ))}
-            </Form.Group>
+              <label className="m-2">Exam Date</label>
+              <input type="text" value={dataRow.examDate} disabled />
+              <label className="m-2">Scores</label>
+              {scoreNew.map((score, i) => (
+                <input
+                  type="number"
+                  value={score}
+                  onChange={(e) => handleSaveNewScore(e, i)}
+                  key={i}
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="m-2 btn btn-secondary"
+              onClick={handleClose}
+            >
+              Cancel
+            </button>
+            <button className="m-2 btn btn-danger" type="submit">
+              Save
+            </button>
           </div>
         </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button
-          variant="primary"
-          onClick={() => updateDataId && handleSave(updateDataId)}
-        >
-          Save Changes
-        </Button>
-      </Modal.Footer>
+      </ModalBody>
     </Modal>
   );
 }
