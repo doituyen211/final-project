76b5550d779cb3432d9ffb9bc9bc60c278cf33f9
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-08-30 15:25:00 +0700

    Add entity no relationship

---
+++ education/src/main/java/org/green/education/entity/ClassSchedule.java
@@ -0,0 +1,28 @@
+package org.green.education.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+@Entity
+public class ClassSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer subjectId;
+    private String time;
+    private Date startTime;
+    private Date endTime;
+    private Integer classId;
+    private String classroom;
+    private Integer staffId;
+
+//    @ManyToOne
+//    @JoinColumn(name = "subject_id", referencedColumnName = "subject_id", foreignKey = @ForeignKey(name = "fk_subject_id_class_schedule"))
+//    private Subject subject;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "class_id", referencedColumnName = "class_id", foreignKey = @ForeignKey(name = "fk_class_id_class_schedule"))
+//    private Class classEntity;
+}
