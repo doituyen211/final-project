9deb8f783de445a52d1eb724d17a681e529e58af
tuyendt <tuyendt@gmail.com>
2024-09-07 16:00:35 +0700

    adding add,update method

--- src/pages/ScoreComponent/index.jsx
+++ src/pages/ScoreComponent/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Table, Card, CardBody, CardHeader } from "react-bootstrap";
+import {
+  Table,
+  Card,
+  CardBody,
+  CardHeader,
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from "react-bootstrap";
 import DropSearch from "./DropSearch";
 import axios from "axios";
 import { BsPencil, BsTrash, BsPlus } from "react-icons/bs";
 
 export default function ScoreComponent() {
   const [gradeData, setGradeData] = React.useState([]);
+  const [dataRow, setDataRow] = React.useState({});
 
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(null);
 
-  const apiUrl = "http://localhost:9001/score";
+  const [show, setShow] = React.useState(false);
+  const [showAdd, setShowAdd] = React.useState(false);
+  const [updateSelectId, setUpdateSelectId] = React.useState(null);
+
+  // const apiUrl = "http://localhost:9001/score";
+  const apiMock = "https://66b2e33c7fba54a5b7eab653.mockapi.io/grades/grade";
 
   React.useEffect(() => {
     fetchData();
@@ -18,13 +32,24 @@ export default function ScoreComponent() {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(apiUrl);
+      const res = await axios.get(apiMock);
       setGradeData(res.data);
       setLoading(false);
     } catch (err) {
       setError(err);
       setLoading(false);
     }
+  };
+
+  const handleShowUpdateForm = (data) => {
+    setShow(true);
+    setUpdateSelectId(data.id);
+    setDataRow(data);
+  };
+
+  const handleShowAddForm = (data) => {
+    setShowAdd(true);
+    setDataRow(data);
   };
 
   if (loading)
@@ -44,16 +69,39 @@ export default function ScoreComponent() {
       </CardHeader>
       <CardBody className="d-flex justify-content-center">
         <div className="container-fluid fullscreen">
-          <button className="m-2 d-flex justify-content-center">
+          <button
+            className="m-2 d-flex justify-content-center"
+            onClick={() => handleShowAddForm(gradeData)}
+          >
             <BsPlus className="text-success"></BsPlus>
           </button>
-          <div className="row ">
+          <div className="row">
             <div className="col-3">
               <DropSearch data={gradeData} />
             </div>
             <div className="col-9">
-              <TableSearchResult data={gradeData} />
+              <TableSearchResult
+                data={gradeData}
+                showUpdateForm={handleShowUpdateForm}
+              />
             </div>
+            {show && updateSelectId && (
+              <UpdateForm
+                scoreData={gradeData}
+                show={show}
+                dataRow={dataRow}
+                handleClose={() => setShow(false)}
+              />
+            )}
+
+            {showAdd && (
+              <AddForm
+                scoreData={gradeData}
+                show={showAdd}
+                dataRow={dataRow}
+                handleClose={() => setShowAdd(false)}
+              />
+            )}
           </div>
         </div>
       </CardBody>
@@ -61,7 +109,7 @@ export default function ScoreComponent() {
   );
 }
 
-function TableSearchResult({ data }) {
+function TableSearchResult({ data, showUpdateForm }) {
   return (
     <div className="container-fluid fullscreen">
       <Card>
@@ -96,7 +144,7 @@ function TableSearchResult({ data }) {
                   <td>aver</td>
                   <td>{grade.status}</td>
                   <td>
-                    <button>
+                    <button onClick={() => showUpdateForm(grade)}>
                       <BsPencil className="text-primary"></BsPencil>
                     </button>
                     <button>
@@ -110,5 +158,95 @@ function TableSearchResult({ data }) {
         </CardBody>
       </Card>
     </div>
+  );
+}
+
+function UpdateForm({ show, handleClose, dataRow }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <ModalHeader closeButton>
+        <span>Update</span>
+      </ModalHeader>
+      <ModalBody>
+        <form>
+          <div className="mb-3">
+            <label className="form-label">Id : </label>
+            <input type="number" value={dataRow.id} disabled></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Name : </label>
+            <input type="text" value={dataRow.studenName} disabled></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Trainning Program : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Subject : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Exam Date : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Score : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Status : </label>
+            <input type="text"></input>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </ModalBody>
+    </Modal>
+  );
+}
+
+function AddForm({ show, handleClose, dataRow }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <ModalHeader closeButton>
+        <span>Add</span>
+      </ModalHeader>
+      <ModalBody>
+        <form>
+          <div className="mb-3">
+            <label className="form-label">Id : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Name : </label>
+            <input type="text"></input>
+          </div>
+          {/* <div className="mb-3">
+            <label className="form-label">Trainning Program : </label>
+            <input type="number"></input>
+          </div> */}
+          {/* <div className="mb-3">
+            <label className="form-label">Subject : </label>
+            <input type="number"></input>
+          </div> */}
+          <div className="mb-3">
+            <label className="form-label">Exam Date : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Score : </label>
+            <input type="number"></input>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Status : </label>
+            <input type="text"></input>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </ModalBody>
+    </Modal>
   );
 }
