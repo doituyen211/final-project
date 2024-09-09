2178d8efaf63938e6caa5b11d1c2886d70fea852
nhanpham220901 <nhanpham220901@gmail.com>
2024-09-09 10:18:46 +0700

    hoan thanh get list,phan trang

--- education/src/main/java/org/green/education/controller/TrainingProgramController.java
+++ education/src/main/java/org/green/education/controller/TrainingProgramController.java
@@ -1,12 +1,17 @@
 package org.green.education.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.green.core.model.CoreResponse;
+import org.green.education.dto.ProgramDTO;
 import org.green.education.service.ITrainingProgramService;
-import org.green.education.service.TrainingProgramService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/training_program")
@@ -15,7 +20,17 @@ public class TrainingProgramController {
     private final ITrainingProgramService trainingProgramService;
 
     @GetMapping("")
-    public ResponseEntity<?> getAllTrainingPrograms() {
-        return ResponseEntity.ok(trainingProgramService.getAllTrainingPrograms());
+    public CoreResponse<?> getAllTrainingPrograms(@RequestParam(value = "page") int page, @RequestParam("limit") int limit) {
+        return trainingProgramService.getAllTrainingPrograms(page, limit);
+    }
+
+    @GetMapping("/getListClassByProgramId/{id}")
+    public ResponseEntity<?> getListClassByProgramId(@PathVariable int id, @RequestParam(value = "page") int page, @RequestParam("limit") int limit) {
+        return ResponseEntity.ok(trainingProgramService.getClassesByTrainingProgramId(id, page, limit));
+    }
+
+    @GetMapping("/getListSubjectByProgramId/{id}")
+    public ResponseEntity<?> getListSubjectByProgramId(@PathVariable int id, @RequestParam(value = "page") int page, @RequestParam("limit") int limit) {
+        return ResponseEntity.ok(trainingProgramService.getSubjectsByTrainingProgramId(id, page, limit));
     }
 }
